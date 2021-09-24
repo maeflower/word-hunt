00a7fe9206b771167ce5bc9bf839ee4f798ddf51
Maeflower <maeflower@anniecannons.com>
2021-09-24 10:03:39 -0700

    second commit---test

--- src/Components/Definitions/Definitions.js
+++ src/Components/Definitions/Definitions.js
@@ -20,14 +20,19 @@ const Definitions = ({word, category,meanings, lightMode}) => {
            {word === "" ? (
            <span className='subTitle'> Start by typing a word in Search</span>
            ) : (
+            //  creating a map that iterates through the meanings array and changes the data being pull to mean. It renders a list of means to the DOM. Mean is the callback and now represents each meaning every time it is executed.
             meanings.map((mean)=> (
+            // another map is create to iterate through all of the items inside of the new array of means and are is rendering a list call item to the DOM. Item is the callback and now represents each meaning every time it is executed.
                 mean.meanings.map((item)=>(
+            // we are not mapping through the items of the mean to get the definitions of each item in the mean array.
                    item.definitions.map((def) => (
+                    //    I'm returning a div with some embedded styling and calling the for the items that are being mapped through to display inside the curly braces.
                        <div 
                        className='singleMean' 
                     //    in styling I'm checking to see if lightMode exist and if it does I want the screen to be a certain color if not it will be white.
                        style={{backgroundColor: lightMode? '#3b5360' : 'white', 
                        color:lightMode? 'white' :'black'}}>
+                           
                            <b>{def.definition}</b>
                            <hr style={{backgroundColor:'black', width:'100%'}}/>
                            {
